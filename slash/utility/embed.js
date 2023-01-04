55e12ee7e5e91e87f62def48aa4b00f6afb2573b
Int Fract <110207136+intfract@users.noreply.github.com>
2023-01-04 21:48:59 +0000

    added safety checks to embed command

--- slash/utility/embed.js
+++ slash/utility/embed.js
@@ -55,7 +55,13 @@ module.exports = {
     const author = guild.members.cache.get(user.id)
     let color = interaction.options.get('color')
     let hex = '#2f3136'
-    if (color && /^#([0-9A-F]{3}){1,2}$/i.test(color.value)) hex = color.value
+    if (color) {
+      if (/^([0-9A-F]{6}){1,2}$/i.test(color.value)) {
+        hex = '#' + color.value
+      } else if (/^#([0-9A-F]{6}){1,2}$/i.test(color.value)) {
+        hex = color.value
+      }
+    }
     
     const embed = new EmbedBuilder()
       .setTitle(`${interaction.options.get('title').value}`)
@@ -75,9 +81,12 @@ module.exports = {
           if (['Author', 'Footer'].includes(x)) {
             v = {}
             v.name = y[0]
-            v.iconURL = y[1]
-            v.url = y[2]
-            // check empty strings and invalid URLs
+            if (y[1] && /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/i.test(y[1])) {
+              v.iconURL = y[1]
+            }
+            if (y[2] && /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(y[2])) {
+              v.url = y[2]
+            }
           } else {
             v = []
             for (let s of y) {
